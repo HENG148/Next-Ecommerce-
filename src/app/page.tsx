101fb7ba32306979cd448c09f0465bef79246a9f
R.Sokheng <support@giantfocus.com>
2025-02-05 13:10:21 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import { products } from "../../utils/products";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
-import { truncateText } from "../../utils/truncateText";
 import ProductCard from "./components/products/ProductCard";
-
-
 
 export default function Home() {
   return (
